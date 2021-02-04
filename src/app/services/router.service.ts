74c40c3c049d70caa0bd12370a4b46964e0c162c
ValentinBabin <valentin.babin@hotmail.fr>
2021-02-05 00:54:13 +0100

    Add method for back button

--- src/app/services/router.service.ts
+++ src/app/services/router.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouterService {
 
-  constructor(
-    private readonly router: Router
-  ) { }
+  private history: string[] = []
 
-  navigateTo(url: string): void {
+  constructor(
+    private readonly router: Router,
+    private readonly location: Location) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.history.push(event.urlAfterRedirects)
+      }
+    })
+  }
+
+  public navigateTo(url: string): void {
     this.router.navigateByUrl(url);
+  }
+
+  public navigateToBackPage(): void {
+    this.history.pop();
+    if (this.history.length > 0) {
+      this.location.back();
+    } else {
+      this.router.navigateByUrl('/');
+    }
   }
 }
