840c72a50d1f719249c7e7a6688ddf3ab8667be7
Valentin Babin <44226732+ValentinBabin@users.noreply.github.com>
2021-02-13 13:14:02 +0100

    Update price.pipe.ts

--- src/app/pipes/price.pipe.ts
+++ src/app/pipes/price.pipe.ts
@@ -5,11 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PricePipe implements PipeTransform {
 
-  transform(price: number, ...args: unknown[]): string {
+  transform(price: number, currencyCode: string, ...args: unknown[]): string {
     if (price <= 0) {
       return 'Prix inconnu';
     } else {
-      return `${price} €`;
+      return (currencyCode === 'EUR') ? `${price * 0.82} €` : `${price} $`;
     }
   }
 
