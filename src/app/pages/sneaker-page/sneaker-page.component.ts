c719d877d74ae65b2afefe795b49f3524d7eb464
Valentin Babin <44226732+ValentinBabin@users.noreply.github.com>
2021-02-13 11:10:44 +0100

    Update sneaker-page.component.ts

--- src/app/pages/sneaker-page/sneaker-page.component.ts
+++ src/app/pages/sneaker-page/sneaker-page.component.ts
@@ -4,7 +4,6 @@ import { ActivatedRoute } from '@angular/router';
 import { Sneaker } from 'src/app/class/sneaker';
 import { APISneakerDatabaseService } from 'src/app/services/apisneaker-database.service';
 import { RouterService } from 'src/app/services/router.service';
-import { ScrappingService } from 'src/app/services/scrapping.service';
 // import puppeteer from 'puppeteer';
 
 @Component({
@@ -19,7 +18,6 @@ export class SneakerPageComponent implements OnInit {
 
   constructor(
     private readonly APISneakerDatabaseService: APISneakerDatabaseService,
-    private readonly scrappingService: ScrappingService,
     private readonly route: ActivatedRoute
   ) { }
 
@@ -27,36 +25,7 @@ export class SneakerPageComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     this.sneaker = await this.APISneakerDatabaseService.getSneaker(id);
 
-    this.scrappingService.init();
-
-    // Appelle la fonction getData() et affichage les données retournées
-    // this.getData().then(value => {
-    //   console.log(value)
-    // })
+    console.log(this.sneaker);
   }
-
-  // async getData() {
-  //   // 1 - Créer une instance de navigateur
-  //   const browser = await puppeteer.launch({ headless: false })
-  //   const page = await browser.newPage()
-
-  //   // 2 - Naviguer jusqu'à l'URL cible
-  //   await page.goto('http://books.toscrape.com/')
-  //   await page.click(
-  //     '#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(1) > article > div.image_container > a > img',
-  //   )
-  //   await page.waitFor(1000) // fait une pause d'une seconde
-
-  //   // 3 - Récupérer les données
-  //   const result = await page.evaluate(() => {
-  //     let title = document.querySelector('h1').innerText
-  //     let price = document.querySelector('.price_color')
-  //     return { title, price }
-  //   })
-
-  //   // 4 - Retourner les données (et fermer le navigateur)
-  //   browser.close()
-  //   return result
-  // }
 
 }
