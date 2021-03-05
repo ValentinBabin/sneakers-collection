73b1be6c27fbb1021b3b9d3f94aa4e4eb895e353
Valentin Babin <44226732+ValentinBabin@users.noreply.github.com>
2021-03-05 07:56:52 +0100

    #1 Update sneaker page

--- src/app/pages/sneaker-page/sneaker-page.component.ts
+++ src/app/pages/sneaker-page/sneaker-page.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Sneaker } from 'src/app/class/sneaker';
 import { APISneakerDatabaseService } from 'src/app/services/apisneaker-database.service';
 import { RouterService } from 'src/app/services/router.service';
+import { WebApiService } from 'src/app/services/web-api.service';
 // import puppeteer from 'puppeteer';
 
 @Component({
@@ -15,17 +16,69 @@ export class SneakerPageComponent implements OnInit {
 
   sneaker: Sneaker;
   resellPrice: string;
+  isInCollection: boolean;
+  isInWishlist: boolean;
 
   constructor(
     private readonly APISneakerDatabaseService: APISneakerDatabaseService,
-    private readonly route: ActivatedRoute
+    private readonly route: ActivatedRoute,
+    private readonly webApiService: WebApiService
   ) { }
 
   async ngOnInit(): Promise<void> {
     const id = this.route.snapshot.paramMap.get('id');
     this.sneaker = await this.APISneakerDatabaseService.getSneaker(id);
+    this.webApiService.checkIfSneakerIsIn(id, WebApiService.NAME_API_COLLECTION).subscribe(res => {
+      this.isInCollection = (!res) ? false : true;
+      // console.log(this.isInCollection);
+    });
+    this.webApiService.checkIfSneakerIsIn(id, WebApiService.NAME_API_WISHLIST).subscribe(res => {
+      this.isInWishlist = (!res) ? false : true;
+      // console.log(this.isInWishlist);
+    });
 
     console.log(this.sneaker);
+
+  }
+
+  public addSneakerToCollection(): void {
+    this.isInCollection = true;
+    if (this.isInWishlist) {
+      this.isInWishlist = false;
+      this.webApiService.deleteSneaker(this.sneaker.id, WebApiService.NAME_API_WISHLIST).subscribe(data => {
+        // TODO: BUG PARSING
+        console.log(JSON.stringify(data));
+      });
+    }
+
+    this.webApiService.createSneaker(this.sneaker, WebApiService.NAME_API_COLLECTION).subscribe(data => {
+      // TODO: BUG PARSING
+      console.log(JSON.stringify(data));
+    });
+  }
+
+  public addSneakerToWishlist(): void {
+    this.isInWishlist = true;
+    this.webApiService.createSneaker(this.sneaker, WebApiService.NAME_API_WISHLIST).subscribe(data => {
+      // TODO: BUG PARSING
+      console.log(JSON.stringify(data));
+    });
+  }
+
+  public deleteSneakerToCollection(): void {
+    this.isInCollection = false;
+    this.webApiService.deleteSneaker(this.sneaker.id, WebApiService.NAME_API_COLLECTION).subscribe(data => {
+      // TODO: BUG PARSING
+      console.log(JSON.stringify(data));
+    });
+  }
+
+  public deleteSneakerToWishlist(): void {
+    this.isInWishlist = false;
+    this.webApiService.deleteSneaker(this.sneaker.id, WebApiService.NAME_API_WISHLIST).subscribe(data => {
+      // TODO: BUG PARSING
+      console.log(JSON.stringify(data));
+    });
   }
 
 }
